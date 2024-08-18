ba2a0b097a2a9787173d3a8b738541a2479ba038
David <nevermore6264@example.com>
2024-08-18 10:39:28 +0700

    Update css

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer";
 import Game from "../components/Game";
 
 export default function Home() {
   return (
     <div>
       <Game />
+      <Footer />
     </div>
   );
 }
---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2024 React Plus Group. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
